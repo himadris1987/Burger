850cbe5f144a51be2cb30ed74bfeaec313c02d29
Himadri <himadris@gmail.com>
2020-07-03 20:07:11 -0500

    updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, eaten, date_time)
-VALUES ("Big Mac", false, "CURRENT_TIMESTAMP"),
-("Baconater", false, "CURRENT_TIMESTAMP"),
-("Sourdough Jack", false, "CURRENT_TIMESTAMP");
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", false),
+("Baconater", false),
+("Sourdough Jack", false);
